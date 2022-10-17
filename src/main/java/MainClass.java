0d6c09d62037e6b90e4f808dcf0c667c0da93f88
n.polyakova <natli.d26.l8@gmail.com>
2022-10-17 17:23:40 +0300

    Add new field and change constructor

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,11 @@
 public class MainClass {
 
+    String someString;
     public int returnSomeValue(int a) {
         return a*2 + 3;
+    }
+
+    public MainClass(String b){
+        someString = b;
     }
 }
